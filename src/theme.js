cbc15d9a99e775d5030c66a6692ae6b68d32b2ec
ajmothossain2002 <111175106+ajmothossain2002@users.noreply.github.com>
2025-07-13 15:38:54 +0530

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
     
   typography: {
-    fontFamily: `'quintessential', sans-serif`,
+    fontFamily: `'Quintessential', serif`,
   },
    palette: {
     mode: 'light',
